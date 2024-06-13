299fb35308eee7f15da5e84ef35bfae75e1c9943
nt54hamnghi <hamnghi250699@gmail.com>
2024-06-12 21:24:11 -0400

    improve error message and add WriteFile function

--- util/core.go
+++ util/core.go
@@ -3,15 +3,36 @@ package util
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 )
 
+// region: --- file operations
+
+func WriteFile(filename string, msg string) error {
+	content := []byte(msg)
+
+	// write to file with 0644 permission
+	// 0644 means owner can read and write, group can read, and others can read
+	// writes data to the named file, creating it if necessary, truncate it if it already exists
+	if err := os.WriteFile(filename, content, 0644); err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+
+	return nil
+}
+
+// endregion: --- file operations
+
+// region: --- http operations
+
 func Get[T any](url string, headers map[string][]string) (T, error) {
-	return do[T]("GET", url, nil, headers)
+	return do[T](http.MethodGet, url, nil, headers)
 }
 
 func Post[T any](url string, body any, headers map[string][]string) (T, error) {
-	return do[T]("POST", url, body, headers)
+	return do[T](http.MethodPost, url, body, headers)
 }
 
 func do[T any](method string, url string, body any, headers map[string][]string) (T, error) {
@@ -20,13 +41,13 @@ func do[T any](method string, url string, body any, headers map[string][]string)
 	// convert struct to into a JSON-encoded byte slice
 	bodyBytes, err := json.Marshal(body)
 	if err != nil {
-		return res, err
+		return res, fmt.Errorf("failed to marshal body: %w", err)
 	}
 
 	// wrap the JSON byte slice in a `*bytes.Buffer` so it can be read by the HTTP request body.
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(bodyBytes))
 	if err != nil {
-		return res, err
+		return res, fmt.Errorf("failed to create request: %w", err)
 	}
 	req.Header = headers
 
@@ -34,15 +55,17 @@ func do[T any](method string, url string, body any, headers map[string][]string)
 	client := &http.Client{}
 	rawResp, err := client.Do(req)
 	if err != nil {
-		return res, err
+		return res, fmt.Errorf("failed to send request: %w", err)
 	}
 	defer rawResp.Body.Close()
 
 	// decode the JSON response into a struct
 	err = json.NewDecoder(rawResp.Body).Decode(&res)
 	if err != nil {
-		return res, err
+		return res, fmt.Errorf("failed to decode response: %w", err)
 	}
 
-	return res, err
+	return res, nil
 }
+
+// endregion: --- http operations
