dd09931b474f3c84f1807e6606618cff6c6e89e5
nt54hamnghi <hamnghi250699@gmail.com>
2024-06-16 22:08:44 -0400

    implement caption subcommand

--- cmd/caption.go
+++ cmd/caption.go
@@ -4,8 +4,11 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	"github.com/nt54hamnghi/hoc/youtube"
 	"github.com/spf13/cobra"
 )
 
@@ -13,9 +16,27 @@ import (
 var captionCmd = &cobra.Command{
 	Use:     "caption",
 	Short:   "Get caption from a YouTube video",
-	Aliases: []string{"c"},
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("caption called")
+	Aliases: []string{"c", "cap"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return fmt.Errorf("expecting 1 argument, got %d", len(args))
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		caption, err := youtube.WithVideoUrl(ctx, args[0])
+		if err != nil && err.Error() == "invalid YouTube video URL" {
+			caption, err = youtube.WithVideoId(ctx, args[0])
+		}
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(caption)
+
+		return nil
 	},
 }
 
