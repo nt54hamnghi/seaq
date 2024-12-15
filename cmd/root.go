0bf658e3292e0bc4e08c00b6898d52219d74c32e
nt54hamnghi <hamnghi250699@gmail.com>
2024-12-14 21:03:02 -0500

    bump version to 0.1.3

--- cmd/root.go
+++ cmd/root.go
@@ -51,7 +51,7 @@ var (
 var rootCmd = &cobra.Command{
 	Use:          "hiku",
 	Short:        "A cli tool to make learning more fun",
-	Version:      "0.1.2",
+	Version:      "0.1.3",
 	Args:         cobra.NoArgs,
 	SilenceUsage: true,
 	PreRunE:      flaggroup.ValidateGroups(&output),
